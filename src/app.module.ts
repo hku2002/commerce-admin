ab7bcd63e7c1973f503ef14db1c9807289ddcf5c
Hwang <hku2002@hanmail.net>
2022-12-29 22:04:16 +0900

    feat: auth 모듈 최초 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { ProductModule } from './product/product.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './common/configs/typeorm.config';
 import { ItemModule } from './item/item.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), ProductModule, ItemModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), ProductModule, ItemModule, AuthModule],
 })
 export class AppModule {}
